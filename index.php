f3d6df4df8f50ee3586ddc2452dc0ffcbf9d331e
Friski Pradana <49704585+friskipradana@users.noreply.github.com>
2020-01-04 09:52:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
 		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 			<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
-				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/">Home</a>
+				<a class="nav-link" href="https://subazurewebapp.azurewebsites.net">Home</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="https://smart-parkir.azurewebsites.net/analyze.php">Analisis Kendaraan<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="https://subazurewebapp.azurewebsites.net/analyze.php">Analisis Gambar<span class="sr-only">(current)</span></a>
 			</li>
 		</div>
 		</nav>
